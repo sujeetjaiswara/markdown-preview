9277dc82d2fef3fd954406194e1baee943b5f84d
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-02-03 01:04:37 +0530

    feat: integrate Angular Material components and update styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,11 +7,12 @@ import {
 } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(withEventReplay()), provideAnimationsAsync(),
   ],
 };
