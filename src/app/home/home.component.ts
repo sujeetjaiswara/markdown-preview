2e31d86b6a49f01be6a5cf3e24bb275c08127523
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-02-03 01:30:57 +0530

    feat: add marked library for markdown parsing and update home component for input handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { marked } from 'marked';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  inputContent: string = '';
+  outputContent: string = '';
 
+  async onInputText() {
+    const html = await marked.parse(this.inputContent);
+    this.outputContent = html;
+  }
 }
