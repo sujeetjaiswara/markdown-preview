de4f8937a8eddf6eef1ae2790e5f0b16a1b8dab1
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-04-04 01:27:54 +0530

    feat: add sharing functionality to the markdown editor with fallback options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,50 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'markdown-preview';
+
+  async shareContent(title:string, text:string, url:string) {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: title,
+          text: text,
+          url: url,
+        });
+        console.log('Content shared successfully');
+      } catch (error) {
+        console.error('Error sharing content:', error);
+        // Handle the error (e.g., show a message to the user)
+      }
+    } else {
+      // Web Share API is not supported, provide fallback options
+      console.log('Web Share API not supported, using fallback.');
+      this.fallbackShare(title, text, url); // Call your fallback function
+    }
+  }
+
+  fallbackShare(title: string, text: string, url: string) {
+    this.copyToClipboard(url);
+  }
+
+  async copyToClipboard(text: string) {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('Text copied to clipboard');
+      alert('Link copied to clipboard!');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+      alert('Failed to copy, please copy manually');
+    }
+  }
 }
