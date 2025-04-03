de6ce7b5aec59e5b2abe990cf9682c5405f5d780
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-04-04 01:38:11 +0530

    feat: enhance home component with OnPush change detection and add placeholder to textarea

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { marked } from 'marked';
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule],
+  imports: [FormsModule,MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
   inputContent: string = '';
-  outputContent: string = '';
+  outputContent: string =
+    '<mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="home"></mat-icon>';
 
   async onInputText() {
     const html = await marked.parse(this.inputContent);
-    // const tokens = marked.lexer(this.inputContent);
-    // const html = marked.parser(tokens);
     this.outputContent = html;
   }
 }
