86010bdb2797014106b6423f45348032917dea37
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-04-04 01:50:02 +0530

    feat: implement SafeHtmlPipe for secure HTML rendering and update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { marked } from 'marked';
+import { SafeHtmlPipe } from "../shared/pipes/safe-html.pipe";
 
 @Component({
   selector: 'app-home',
-  imports: [FormsModule,MatIconModule],
+  imports: [FormsModule, MatIconModule, SafeHtmlPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,7 +14,7 @@ import { marked } from 'marked';
 export class HomeComponent {
   inputContent: string = '';
   outputContent: string =
-    '<mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="home"></mat-icon>';
+    `<mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="home"></mat-icon>`;
 
   async onInputText() {
     const html = await marked.parse(this.inputContent);
---
+++ src/app/shared/pipes/safe-html.pipe.ts
@@ -0,0 +1,13 @@
+import { inject, Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'safeHtml',
+})
+export class SafeHtmlPipe implements PipeTransform {
+  private sanitizer = inject(DomSanitizer);
+
+  transform(value: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(value);
+  }
+}
