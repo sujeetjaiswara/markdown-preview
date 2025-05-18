e62f8435a04dfd43381f4b897659910b3e5b3bcb
Sujeet Jaiswara <sujeetjaiswara2012@gmail.com>
2025-05-19 00:17:21 +0530

    Add application icons and manifest for PWA support
    - Added new PNG icons in various sizes (72x72, 96x96) to the public/icons directory.
    - Created a web manifest file (manifest.webmanifest) to define the app's metadata, including name, theme color, background color, display mode, and icon references for different sizes.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import {
@@ -7,12 +11,18 @@ import {
 } from '@angular/platform-browser';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes,withViewTransitions()),
-    provideClientHydration(withEventReplay()), provideAnimationsAsync(),
+    provideRouter(routes, withViewTransitions()),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
